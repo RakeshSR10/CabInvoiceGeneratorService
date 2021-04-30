c3d1cad54b8300a501f627586a1d9d171e7461ce
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-30 22:56:18 +0530

    Added: TotalNumber of Rides, TotalFare, AverageFare per Ride

--- src/test/java/com/cabInvoice/InvoiceServiceTest.java
+++ src/test/java/com/cabInvoice/InvoiceServiceTest.java
@@ -23,19 +23,31 @@ public class InvoiceServiceTest {
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(5, fare);
+        Assertions.assertEquals(5, fare,0.0);
         System.out.println(fare);
     }
 
     //Step 2 Multiple Rides
     //Test 3 CalculateFare for multiple Rides
-    @Test
+    /*@Test
     public void givenMultipleRides_ShouldReturnTotalFare(){
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                         new Ride(0.1,1)};
         double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30,fare);
+        Assertions.assertEquals(30,fare,0.0);
         System.out.println(fare);
+    }*/
+
+    //Step 3 Enhanced Invoice
+    //Test 4 TotalNumber of Rides, TotalFare, AverageFare per Ride
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                        new Ride(0.1,1)};
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary =  new InvoiceSummary(2,30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 }
